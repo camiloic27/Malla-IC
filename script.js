eee2b55b8535cd41df80d9330806cfbdc61d0cac
camiloic27 <iagoinsuco@gmail.com>
2025-07-14 19:43:32 -0400

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("🎯 script.js cargado");
 const cursos = [
   // SEMESTRE I
   { nombre: "INTRODUCCIÓN A LA PROGRAMACIÓN", sigla: "IIC1103", creditos: 10, semestre: 1, tipo: "economia" },
